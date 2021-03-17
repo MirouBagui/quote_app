5ee83d0f4d5c3e0235a35f2f57d2a978e5e35b6d
Saoudi <saoudi.amire@gmail.com>
2021-03-17 18:37:05 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.scss';
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import {FaTwitter} from 'react-icons/fa'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        quoteData: [],
+        quote: '',
+        author: ''
+      }
+      this.randomQuote = this.randomQuote.bind(this);
+      this.handleClick = this.handleClick.bind(this);
+    }
+  // thanx to Akhil Aravind https://stackoverflow.com/questions/59715034/react-fetch-api-quote-random-generator?fbclid=IwAR2yd-PADcCjgq0vKO-rusF_gGNSrCgrjnmbLclE8hYNVyNi_3oa7Hbu1G4
+    componentDidMount() {
+      console.log("COM D Mount")
+      const API = 'https://type.fit/api/quotes'
+      fetch(API)
+          .then((response) => response.json())
+          .then((data) => {
+              this.setState({
+                quoteData: data
+              },()=>{
+                this.handleClick();
+              })
+          })
+          .catch(error => console.log('Error', error));
+    }
+    randomQuote() {
+      const randomNumber = Math.floor(Math.random() * this.state.quoteData.length);
+      console.log(randomNumber);
+      return this.state.quoteData[randomNumber];
+    }
+
+  
+    handleClick() {
+      const oneRandomQuote = this.randomQuote();
+      this.setState({
+        quote: oneRandomQuote.text,
+        author: oneRandomQuote.author
+      })
+    }
+  
+    render() {
+      return (
+      <Jumbotron className="content" fluid>
+          <Container fluid id='quote-box'>
+            <h3 id='author'>
+             ---<b>|</b> {this.state.author} <b>|</b>---
+            </h3>
+            <h1 id='text'>
+              {this.state.quote}
+            </h1> 
+            <hr />
+            <a id="tweet-quote" className="bg-info text-white" href="#top" target="_blank"> <FaTwitter size='25'/>the quote</a> <br />
+            <Button variant="outline-success" size="sm" id="new-quote" onClick={this.handleClick}>Next Quote</Button>
+          </Container>
+      </Jumbotron>
+      )
+    }
+  }
